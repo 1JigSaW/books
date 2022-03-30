9245945922f093d0fa946e99072a458453784e7e
1JigSaW <misho_1998@mail.ru>
2022-03-30 22:40:46 +0300

    ...

--- Problem_Solving_with_Algorithms_and_Data_Structures/Introduction/Ex15.py
+++ Problem_Solving_with_Algorithms_and_Data_Structures/Introduction/Ex15.py
@@ -1,39 +1,45 @@
-task = [[' ', '8', ' ', '6', ' ', ' ', ' ', '7', ' '],
-		['3', ' ', ' ', '1', ' ', ' ', ' ', ' ', '2'],
-		[' ', ' ', '4', ' ', '3', ' ', '6', ' ', ' '],
-		[' ', ' ', ' ', ' ', ' ', '6', ' ', '5', '4'],
-		[' ', ' ', '8', ' ', '2', ' ', '9', ' ', ' '],
-		['5', '1', ' ', '7', ' ', ' ', ' ', ' ', ' '],
-		[' ', ' ', '2', ' ', '7', ' ', '4', ' ', ' '],
-		['8', ' ', ' ', ' ', ' ', '3', ' ', ' ', '9'],
-		[' ', '9', ' ', ' ', ' ', '5', ' ', '1', ' ']]
+task = [[' ', '8', ' '],
+		['3', ' ', ' '],
+		[' ', ' ', '4']]
 
 
-n = 3
-for col in range(len(task) + 1):
+arr = []
+for col in range(len(task)):
 	for k in range(len(task[0])):
 		exp_arr = []
-		for line in task[:n]:
-			for num in line[:n]:
-				if num != ' ':
-					exp_arr.append(num)
-
-		for line in task[:1]:
-			for num in line:
-				if num != ' ':
-					exp_arr.append(num)
-
-		for line in task:
-			for num in line[:1]:
-				if num != ' ':
-					exp_arr.append(num)
-
-
-		exp_arr = list(set(exp_arr))
-
 		use = []
-		for j in range(1,10):
-			if str(j) not in exp_arr:
-				use.append(j)
-		print(exp_arr)
-		print(use)
+		if task[col][k] == ' ':
+
+			for line in task[col:col+1]:
+				for num in line:
+					if num != ' ':
+						exp_arr.append(num)
+
+
+			for line in task:
+				for num in line[k:k+1]:
+					if num != ' ':
+						exp_arr.append(num)
+
+
+
+			exp_arr = list(set(exp_arr))
+
+			for j in range(1,10):
+				if str(j) not in exp_arr:
+					use.append(j)
+			print(use)
+			
+			arr.append(use)
+		else:
+			arr.append(use)
+print(arr)
+
+k = 0
+for i in range(len(task)):
+	for j in range(len(task[i])):
+		if task[i][j] == ' ':
+			task[i][j] = arr[k][0]
+			k += 1
+
+print(len(task))
