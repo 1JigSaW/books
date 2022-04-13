54db111d7303926ad9cbb9563994bb0f587d460d
1JigSaW <misho_1998@mail.ru>
2022-04-13 23:58:53 +0300

    tasks

---
+++ Problem_Solving_with_Algorithms_and_Data_Structures/Recursion/Ex6.py
@@ -0,0 +1,40 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+
+l1 = Stack()
+l2 = Stack()
+l3 = Stack()
+l1.push(1)
+l1.push(1)
+l1.push(1)
+
+def moveTower(height,fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+
+def moveDisk(fp,tp):
+    fp.pop()
+    tp.push(1)
+
+
+moveTower(3,l1,l2,l3)
